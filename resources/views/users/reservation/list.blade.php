c0556be669683d39677bd23942b4ee1cf4db8cdf
Masahiro SUYAMA <suyama@mx4.mesh.ne.jp>
2024-04-19 00:07:29 +0900

    create UI of Reservation List

--- resources/views/users/reservation/list.blade.php
+++ resources/views/users/reservation/list.blade.php
@@ -57,31 +57,71 @@
     }
 </style>
 
-<div class="container w-75">
-    <h2 class="text-center font-navy font-bold py-5"><i class="fa-solid fa-exclamation"></i> Reservation Confirmation</h2>
-    <div class="mt-3 py-3 details-box">
-        <div class="w-75">
-            <h4 class="py-2 font-bold">Choose Type: </h4>
-            <h4 class="py-3 font-bold">Details: </h4>
-
-            <ul class="record-list">
-                @foreach ($tentativeAllReservations as $reservation)
-                    <li>
-                        <span>{{ $reservation['date'] }}</span>
-                        <span>{{ $reservation['area'] }}</span>
-                        <span>{{ $reservation['fee'] }}</span>
-                    </li>
-                @endforeach
-            </ul>
-            <div class="total-info">
-                <span>Total:</span>
-                <span>${{ array_sum(array_column($tentativeAllReservations, 'fee')) }}</span>
-            </div>
-            <div class="mt-5 button-group">
-                <button type="button" class="btn btn-cancel me-2">Cancel</button>
-                <button type="button" class="btn btn-confirm">Confirm</button>
+<div class="container w-80 mt-3">
+    <div class="row mb-2">
+        <div class="col">
+            <h4 class="float-start"><i class="fa-solid fa-list"></i>Reservation List</h4>
+            <div class="float-end">
+                <select name="list" id="list" class="form-select" >
+                    <option value="all">All</option>
+                    <option value="upcoming_reservations_all">Upcoming Reservations(All)</option>
+                    <option value="upcoming_reservations_one_month">Upcoming Reservations(One Month)</option>
+                    <option value="upcoming_reservations_one_week">Upcoming Reservations(One Week)</option>
+                    <option value="past_reservations_all">Past Reservations(All)</option>
+                    <option value="past_reservations_one_month">Past Reservations(One Month)</option>
+                    <option value="past_reservations_one_week">Past Reservations(One Week)</option>
+                </select>
             </div>
         </div>
     </div>
-</div>
+    <!-- Table -->
+    <table class="table">
+        <thead>
+            <tr class="table-info">
+                <th scope="col">ID</th>
+                <th scope="col">Area</th>
+                <th scope="col">Date</th>
+                <th scope="col">Type</th>
+                <th scope="col">Fee</th>
+                <th scope="col">PDF Export</th>
+                <th scope="col"><i class="fa-solid fa-download"></i></th>
+                <th scope="col"><i class="fa-solid trash"></i></th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- TODO :  foreach -->
+            <tr>
+                <th scope="row">1</th>
+                <td>Area D, 2F</td>
+                <td>March 18(Sun)</td>
+                <td>Disability</td>
+                <td>$20</td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Area A, 1F</td>
+                <td>March 21(Wed)</td>
+                <td>Disability</td>
+                <td>$20</td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Area A, 1F</td>
+                <td>March 22(Thu)</td>
+                <td>Disability</td>
+                <td>$20</td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Area A, 1F</td>
+                <td>March 23(Fri)</td>
+                <td>Disability</td>
+                <td>$20</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
