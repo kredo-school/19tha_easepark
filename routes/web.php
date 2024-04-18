3878da7d1d6b3634aa48ba036b9801ddd6ba0a16
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-04-18 22:32:59 +0900

    【user/register/registerNewUser】Tentative UI of New Register Updates: - Updated register.blade.php (the action in form tag has been omitted to make the auth feature null for now) - Added the route for showRegistrationFormTest in web.php - Added the function for showRegistrationFormTest in RegisterController.php (This function is A TEST to check the UI of the register page) *Please note that bootstrap and css color for button have not been reflected. (This will be merged by another branch.)

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Route::get('/', function () {
 
 // Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+Route::get('test/registration', [RegisterController::class, 'showRegistrationFormTest']);
