e0eaeb6af615eb2ae1cc748e139c3e67bd2dc605
joacquins <sobrepenacarlo@gmail.com>
2024-04-19 22:03:26 +0800

    【user/reservation/showConfirmationReservation】- Readded showAllConfirmationReservation function
    - Added the removed showAllConfirmationReservation function in ReservationController.php

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,6 +6,17 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
+    public function showAllConfirmationReservation(){
+        $tentativeAllReservations = [
+            ['date' => '2022-04-01', 'area' => 'Area 1', 'fee' => 100],
+            ['date' => '2022-04-02', 'area' => 'Area 2', 'fee' => 200],
+            ['date' => '2022-04-03', 'area' => 'Area 3', 'fee' => 300]
+        ];
+
+        return view('users.reservation.list')
+            -> with('tentativeAllReservations', $tentativeAllReservations);
+    }
+    
     public function showConfirmationReservation()
     {
         $tentativeReservations = [
@@ -14,9 +25,6 @@ class ReservationController extends Controller
             ['date' => '2022-01-03', 'area' => 'Area 3', 'fee' => 300],
         ];
         $userAttribute = 'Disability';
-
-        // return view('users.reservation.list')
-        //     -> with('tentativeAllReservations', $tentativeAllReservations);
 
         return view('users.reservation.confirmation')
             ->with('tentativeReservations', $tentativeReservations)
