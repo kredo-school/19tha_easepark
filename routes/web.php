0e75f61057cd685919eefa30d95fe7845744f16d
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-04-23 18:44:59 +0900

    【admin/manageFees/showRegisteredFees】UI to show registered fees for Admin
    Additions:
    - Added showFess function in FeesController
    - Added UI in show.blade.php
    - Added use for FeesController and route for showFees function in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\FeesController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\Auth\LoginController;
 
@@ -28,4 +29,5 @@ Route::get('/reservation/completion', [ReservationController::class, 'showComple
 //Admin
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/showusers', [UsersController::class, 'showUsers'])->name('showusers');
+    Route::get('/fees/show', [FeesController::class, 'showFees'])->name('fees.show');
 });
