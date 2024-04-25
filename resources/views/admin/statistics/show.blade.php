7f39b04d019ca4b9bf7acb5f1cc045d4e175a065
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-04-25 19:02:01 +0900

    【admin/manageStatistics/showRegisteredAccountNum】Update UI of Statistic Pages Updates: - Added additional css codes in style.css for relevant pages - Modified show.blade.php especially modifying attributes of relevant tags and jquery codes - Added ranges for line chart and included Chart.js codes to output line chart

--- resources/views/admin/statistics/show.blade.php
+++ resources/views/admin/statistics/show.blade.php
@@ -10,58 +10,68 @@
     <div class="card-header bg-light">
         {{-- PartA: This is the container where the tabs for database (users or reservations) are listed. --}}
         <div class="nav nav-tabs statistic-db-choice-range" >
-            {{-- use 'data-bs-target in button tag to specify which element should be visible by clicking which button. --}}
-            <button class="nav-link active statistic-each-db text-black fw-bold" id="users-tab" data-bs-toggle="tab" data-bs-target="#users-sidebar" type="button" role="tab" aria-controls="users" aria-selected="true">Users</button>
+            {{-- use 'data-bs-target in button tag to specify which side bar should be visible by clicking which button. --}}
+            <button class="nav-link active statistic-each-db text-black fw-bold" id="users-tab" data-bs-toggle="tab" data-db-tab-id="users-tab" data-bs-target="#users-sidebar" type="button" role="tab" aria-controls="users" aria-selected="true">Users</button>
 
-            <button class="nav-link statistic-each-db text-black fw-bold" id="reservations-tab" data-bs-toggle="tab" data-bs-target="#reservations-sidebar" type="button" role="tab" aria-controls="reservations" aria-selected="false">Reservations</button>
+            <button class="nav-link statistic-each-db text-black fw-bold" id="reservations-tab" data-bs-toggle="tab"
+            data-db-tab-id="reservations-tab"
+            data-bs-target="#reservations-sidebar" type="button" role="tab" aria-controls="reservations" aria-selected="false">Reservations</button>
         </div>
 
     </div>
     <div class="card-body">
         <div class="row">
-            {{-- PartB: Vertical tab to show statical items of each database. 'id'in this part (users-sidebar/ reservations-sidebar) is associated with 'data-bs-target' in Part A. Thus, when the user clicks the button in Part A, associated elements in Part B shows up.--}}
+            {{-- PartB: Side bar to show statical items of each database. 'id' in this part (users-sidebar/ reservations-sidebar) is associated with 'data-table-id' in Part A. Thus, when the user clicks the button in Part A, associated elements in Part B shows up accordingly.--}}
             <div class="col-md-3 my-md-3">
                 <div class="list-group text-center">
                     <div class="tab-content" id="myTabContent">
                         {{-- Visible if the user clicks the button, 'Users' in Part A --}}
-                        <div class="tab-pane fade show active" id="users-sidebar" role="tabpanel" aria-labelledby="users-tab">
-                            <button class="w-100 list-group-item statistic-extract-condition statistic-extract-condition-clicked" id="registrations-num-tab" data-bs-target="#new-registrations-num" type="button" aria-controls="registrations-num-tab" aria-selected="false">Number of new registrations</button>
+                        <div class="tab-pane fade show active" id="users-sidebar" role="tabpanel" aria-labelledby="users-tab" data-sidebar-id="users-sidebar">
+                            {{-- data-table-id is used for the click handelr to show the target table --}}
+                            <button class="w-100 list-group-item statistic-extract-condition statistic-extract-condition-clicked" id="registrations-num-tab" data-table-id="registrations-num" type="button" aria-controls="registrations-num-tab" aria-selected="false">Number of new registrations</button>
 
-                            <button class="w-100 list-group-item statistic-extract-condition" id="deletions-num-tab" data-bs-target="#deletions-num" type="button" aria-controls="deletions-num-tab" aria-selected="false">Number of deletions</button>
+                            <button class="w-100 list-group-item statistic-extract-condition" id="deletions-num-tab" data-table-id="deletions-num" type="button" aria-controls="deletions-num-tab" aria-selected="false">Number of deletions</button>
                         </div>
                         {{-- Visible if the user clicks the button, 'Reservations' in Part A --}}
-                        <div class="tab-pane fade" id="reservations-sidebar" role="tabpanel" aria-labelledby="reservations-tab">
-                            <button class="w-100 list-group-item statistic-extract-condition statistic-extract-condition-clicked" id="reservations-num-tab" data-bs-target="#reservations-num" type="button" aria-controls="reservations-num-tab" aria-selected="false">Number of reservations</button>
+                        <div class="tab-pane fade" id="reservations-sidebar" role="tabpanel" aria-labelledby="reservations-tab" data-sidebar-id="reservations-sidebar">
+                            <button class="w-100 list-group-item statistic-extract-condition statistic-extract-condition-clicked" id="reservations-num-tab" data-table-id="reservations-num" type="button" aria-controls="reservations-num-tab" aria-selected="false">Number of reservations</button>
 
-                            <button class="w-100 list-group-item statistic-extract-condition" id="cancellations-num-tab" data-bs-target="#cancellations-num" type="button" aria-controls="cancellations-num" aria-selected="false">Number of cancellations</button>
+                            <button class="w-100 list-group-item statistic-extract-condition" id="cancellations-num-tab" data-table-id="cancellations-num" type="button" aria-controls="cancellations-num" aria-selected="false">Number of cancellations</button>
 
-                            <button class="w-100 list-group-item statistic-extract-condition" id="sales-num-tab" data-bs-target="#sales-num" type="button" aria-controls="sales-num" aria-selected="false">Sales</button>
+                            <button class="w-100 list-group-item statistic-extract-condition" id="sales-num-tab" data-table-id="sales-num" type="button" aria-controls="sales-num" aria-selected="false">Sales</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-9">
-                        {{-- Table for Users --}}
-                        <div class="statical-table-range fade show active" id="new-registrations-num" role="tabpanel">
-                            @include('admin.statistics.users.registration')
-                        </div>
+                <div class="form-group w-25">
+                    <select class="form-control year-selection my-2" id="year">
+                        @for ($year = date('Y'); $year >= 2000; $year--)
+                            <option value="{{ $year }}" {{ $year == date('Y') ? 'selected' : '' }}>{{ $year }}</option>
+                        @endfor
+                    </select>
+                </div>
+                {{-- Table for Users --}}
+                <div class="statical-table-range fade show" id="registrations-num" role="tabpanel">
+                    @include('admin.statistics.users.registration')
+                </div>
 
-                        <div class="statical-table-range fade" id="deletions-num" role="tabpanel">
-                            Table to show the number of deletions shows up in here.
-                        </div>
+                <div class="statical-table-range fade" id="deletions-num" role="tabpanel">
+                    Table to show the number of deletions shows up in here.
+                </div>
 
-                        {{-- Table for Reservations --}}
-                        <div class="statical-table-range fade" id="reservations-num" role="tabpanel">
-                            Table to show the number of reservations shows up in here.
-                        </div>
+                {{-- Table for Reservations --}}
+                <div class="statical-table-range fade" id="reservations-num" role="tabpanel">
+                    Table to show the number of reservations shows up in here.
+                </div>
 
-                        <div class="statical-table-range fade" id="cancellations-num" role="tabpanel">
-                            Table to show the number of cancellations shows up in here.
-                        </div>
+                <div class="statical-table-range fade" id="cancellations-num" role="tabpanel">
+                    Table to show the number of cancellations shows up in here.
+                </div>
 
-                        <div class="statical-table-range fade" id="sales-num" role="tabpanel">
-                            Table to show the sales shows up in here.
-                        </div>
+                <div class="statical-table-range fade" id="sales-num" role="tabpanel">
+                    Table to show the sales shows up in here.
+                </div>
             </div>  
         </div>
         
@@ -70,27 +80,107 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('button').on('click', function () {
-                // Get the ID of the target content pane from the 'data-bs-target' attribute
-                var targetContentId = $(this).attr('data-bs-target');
+            
+            // Check if there's a saved tab in localStorage
+            var savedDbTabId = localStorage.getItem('selectedDbTabId');
+            var savedSidebarId = localStorage.getItem('selectedSidebarId');
+            var savedStatisticalTableId = localStorage.getItem('selectedStatisticalTableId');
+            
+            console.log(`savedDbTabId is ${savedDbTabId}, savedSibarId is ${savedSidebarId}, and savedStatisticalTableId is ${savedStatisticalTableId}`);
 
-                // Hide all content panes
+            // Hide all tabs and sidebars initially
+            $('button[data-db-tab-id]').removeClass('active');
+            $('div[data-sidebar-id]').hide();
+            $('button[data-table-id]').removeClass('statistic-extract-condition-clicked');
+            $('.statical-table-range').hide();
+            
+            if (savedDbTabId) {
+                // If there is saved database tab id, show it
+                $('#' + savedDbTabId).addClass('active');
+
+                if (savedSidebarId) {
+                    // If there's a saved sidebar, show it
+                    // $('#' + savedSidebarId).show().addClass('show active');
+                    $('#' + savedSidebarId).slideDown().addClass('show active');
+
+                    $('button[data-table-id]').removeClass('statistic-extract-condition-clicked');
+                    $('.statical-table-range').hide();
+                    if (savedStatisticalTableId) {
+                        // If there's a saved statistical table, show it
+                        $(`button[data-table-id="${savedStatisticalTableId}"]`).addClass('statistic-extract-condition-clicked');
+                        // $('#' + savedStatisticalTableId).show().addClass('show active');
+                        $('#' + savedStatisticalTableId).slideDown().addClass('show active');
+                    }
+                }
+            } else {
+                // If database tab, side-bar, and table are not saved, show the default elements: users tab, users-sidebar, new registration table
+                $('#users-tab').addClass('active');
+                $('#users-sidebar').show().addClass('show');
+                $('#registrations-num-tab').addClass('statistic-extract-condition-clicked');
+                $('#registrations-num').show().addClass('show');
+            }
+            
+
+            // Attach a click event handler to button which has an attribute, "data-db-tab-id" to store which database tab the user selects.
+            $('button[data-db-tab-id]').on('click', function () {
+                // Get the selected database tab that the user selects
+                var selectedDbTabId = $(this).data('db-tab-id');
+                console.log(`The selected database tab id is ${selectedDbTabId}`);
+
+                // Save the selected tab in localStorage
+                localStorage.setItem('selectedDbTabId', selectedDbTabId);
+
+                // Get the value of the 'data-bs-target' attribute of the clicked tab, then, display the designated sidebar whose id is consistent with that value.
+                var targetSidebar = $(this).data('bs-target');
+                // $(targetSidebar).show().addClass('show active');
+                $(targetSidebar).slideDown().addClass('show active');
+            });
+
+            // Attach a click event handler to button which has data-table-id to store the selected table id, hide tables other than the selected one, and show the selected table.
+            $('button[data-table-id]').on('click', function () {
+                // Get the table ID of the selected table from the 'data-table-id' attribute (associated with the id of the selected table)
+                var selectedStatisticalTableId = $(this).data('table-id');
+                console.log(`The selected statistical table id is ${selectedStatisticalTableId}`);
+
+                // Get the sidebar ID of the selected table
+                var selectedSidebarId = $(this).closest('.tab-pane').attr('id');
+                console.log(`The selected sidebar id is ${selectedSidebarId}`)
+
+                // Save the selected table in localStorage
+                localStorage.setItem('selectedStatisticalTableId', selectedStatisticalTableId);
+
+                // Save the selected sidebar in localStorage
+                localStorage.setItem('selectedSidebarId', selectedSidebarId);
+            
+                // Hide all of the tables first
                 $('.statical-table-range').hide();
 
-                // Show the target content pane
-                $(targetContentId).show().addClass('show');
+                // Show the selected table
+                // $('#' + selectedStatisticalTableId).show().addClass('show active');
+                $('#' + selectedStatisticalTableId).slideDown().addClass('show active');
             });
 
-            $('.statistic-extract-condition').on('click', function (event) {
-                // Prevent the default action of the click event (navigating to the href)
-                event.preventDefault();
-
-                // Remove the 'clicked' class from all <a> tags within the #users-sidebar div
+            // Attach a click event handler to buttons which has a class, 'statistic-extract-condition', to highlight the selected button
+            $('.statistic-extract-condition').on('click', function () {
+                // Remove the 'statistic-extract-condition-clicked' class from all <a> tags within the #users-sidebar div
                 $('.statistic-extract-condition').removeClass('statistic-extract-condition-clicked');
 
-                // Add the 'clicked' class to the clicked <a> tag
+                // Add the 'statistic-extract-condition-clicked' class to the clicked <a> tag
                 $(this).addClass('statistic-extract-condition-clicked');
             });
+
+            // Attach a click event handler to the database tab to show the default table when the user moves to anoterh tab
+            
+            // Trigger a click event on the "Number of reservations" button when the user clicks the "Reservations" tab
+            $('#reservations-tab').on('click', function () {
+                $('#reservations-num-tab').trigger('click');
+            });
+            // Trigger a click event on the "Number of reservations" button when the user clicks the "Users" tab
+            $('#users-tab').on('click', function () {
+                $('#registrations-num-tab').trigger('click');
+            });
+
+            
         });
     </script>
 @endsection
--- resources/views/admin/statistics/users/registration.blade.php
+++ resources/views/admin/statistics/users/registration.blade.php
@@ -1,10 +1,3 @@
-<div class="form-group w-25">
-    <select class="form-control my-2" id="year">
-        @for ($year = date('Y'); $year >= 2000; $year--)
-            <option value="{{ $year }}" {{ $year == date('Y') ? 'selected' : '' }}>{{ $year }}</option>
-        @endfor
-    </select>
-</div>
 <div class="table-responsive">
     <table class="table table-hover align-middle border-0">
         <thead class="small table-info">
@@ -26,14 +19,46 @@
             @endforeach
         </tbody>
     </table>
+
+    <div class="chart-container">
+        <canvas id="myChart"></canvas>
+    </div>
 </div>
 
-<style>
-    .form-control {
-        appearance: none;
-        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='5' viewBox='0 0 4 5'%3E%3Cpath fill='%23333' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E");
-        background-repeat: no-repeat;
-        background-position: right .75rem center;
-        background-size: 8px 10px;
-    }
-</style>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var colors = ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(54, 162, 235, 0.2)'];
+    var borderColors = ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 206, 86, 1)', 'rgba(54, 162, 235, 1)'];
+    var datasets = @json($registrationDataByDefault['attributes']).map(function(attribute, index) {
+        return {
+            label: attribute,
+            data: @json($registrationDataByDefault['months']).map(function(month) {
+                return @json($registrationDataByDefault['statisticalData'])[attribute][month] || 0;
+            }),
+            backgroundColor: colors[index % colors.length],
+            borderColor: borderColors[index % borderColors.length],
+            borderWidth: 1
+        };
+    });
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: @json($registrationDataByDefault['months']),
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+</script>
