7161cc771e0a635be739efbac6f9680d44e3e564
Masahiro SUYAMA <suyama@mx4.mesh.ne.jp>
2024-04-26 19:14:04 +0900

    [admin/manageAttributes/showRegisteredAttributes]
    Create
    - views/admin/attributes/show.blade.php
    -/attributesController.php
    
    Add
    -web.php

--- app/Http/Controllers/admin/AttributesController.php
+++ app/Http/Controllers/admin/AttributesController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class AttributesController extends Controller
 {
+    public function showAttribute()
+    {
+        return view('admin.attributes.show');
+    }
+
+
     public function editAttribute()
     {
         return view('admin.attributes.edit');
