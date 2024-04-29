92f1239173d499b07f4c732e40dd2bc3d03c7b12
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-04-29 16:26:09 +0900

    Fixe duplicates in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\admin\AdminsController;
 use App\Http\Controllers\admin\AttributesController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
@@ -17,7 +16,6 @@ Route::get('/', function () {
 //Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/test/login-admin', [LoginController::class, 'adminLogin'])->name('login-admin');
 Route::get('/login', [LoginController::class, 'userLogin'])->name('login');
 
