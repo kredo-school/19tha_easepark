774c4192dd76a83cfe8b6fc3092709a3150d96ea
YOSHIMI <lpit5vg4@gmail.com>
2024-04-30 00:32:43 +0900

    [admin/manageFees/deleteRegisteredFees] - Modal for delete fee
    Added: UI for delete Fees in delete.blade.php(Fees)
    
    Noted:
    - show.blade.php(Fees) ,web.php , FeesController are default

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,16 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\admin\AdminsController;
-use App\Http\Controllers\admin\AttributesController;
 use App\Http\Controllers\admin\FeesController;
-
+use App\Http\Controllers\admin\AttributesController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 //Auth::routes();
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::get('/test/login-admin', [LoginController::class, 'adminLogin'])->name('login-admin');
+Route::get('/test/login-admin', [LoginController::class, 'adminLogin'])->name('login-admin');
 Route::get('/login', [LoginController::class, 'userLogin'])->name('login');
-
 Route::get('/profile/show', [ProfileController::class, 'showProfile'])->name('profile.show');
 Route::get('/profile/edit', [ProfileController::class, 'editProfile'])->name('profile.edit');
 Route::get('/homepage', [HomeController::class, 'homePage'])->name('homepage');
@@ -28,12 +23,11 @@ Route::get('/test/login-admin', [LoginController::class, 'adminLogin'])->name('l
 Route::get('/reservation/list', [ReservationController::class, 'showAllConfirmationReservation'])->name('reservation.list');
 Route::get('/reservation/confirmation', [ReservationController::class, 'showConfirmationReservation'])->name('reservation.confirmation');
 Route::get('/reservation/completion', [ReservationController::class, 'showCompletionReservation'])->name('reservation.completion');
-
 //Admin
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/attribute/edit', [AttributesController::class, 'editAttribute'])->name('admin.attributes.edit');
     Route::get('/showusers', [UsersController::class, 'showUsers'])->name('showusers');
     Route::get('/admins/edit', [AdminsController::class, 'editAdmin'])->name('admins.edit');
     Route::get('/admins/register', [AdminsController::class, 'registerAdmin'])->name('admins.register');
-    Route::get('/fees/edit',[FeesController::class,'updateRegisteredFees'])->name('admin.fees.edit');
+    Route::get('/default',[FeesController::class,'default'])->name('default');
 });
