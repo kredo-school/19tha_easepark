541d58eed19bc787493e9e584333dfc671f1f230
Tomo Furu <to.frkw@ymail.ne.jp>
2024-05-04 15:55:49 +0900

    [deleteRegisteredAttributes] - fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\FeesController;
-use App\Http\Controllers\Admin\AreasController;
-use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\Admin\AdminsController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\AreasController;
 use App\Http\Controllers\Admin\AttributesController;
-use App\Http\Controllers\Admin\StatisticsController;
+use App\Http\Controllers\Admin\FeesController;
+use App\Http\Controllers\Admin\AdminsController;
 use App\Http\Controllers\Admin\ReservationsController;
 
 Route::get('/', function () {
@@ -23,6 +20,9 @@ Route::get('/', function () {
 
 // for Home Page
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/homepage', [HomeController::class, 'homePage'])->name('homepage');
+
+// for Login & Registration
 Route::get('/test/login-admin', [LoginController::class, 'adminLogin'])->name('login-admin');
 Route::get('/login', [LoginController::class, 'userLogin'])->name('login');
 Route::get('/test/registration', [RegisterController::class, 'showRegistrationFormTest']);
@@ -38,25 +38,26 @@ Route::get('/reservation/completion', [ReservationController::class, 'showComple
 
 //For Admin
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    // For Users
+    Route::get('/users/show', [UsersController::class, 'showUsers'])->name('users.show');
+
+    // For Attributes
     Route::get('/attribute/show', [AttributesController::class, 'showAttribute'])->name('attributes.show');
     Route::get('/attribute/edit', [AttributesController::class, 'editAttribute'])->name('attributes.edit');
-    Route::get('/admins/edit', [AdminsController::class, 'editAdmin'])->name('edit');
-    Route::get('/admins/register', [AdminsController::class, 'registerAdmin'])->name('register');
-    Route::get('/users/show', [UsersController::class, 'showUsers'])->name('users.show');
+
+    //For Admins
+    Route::get('/admins/register', [AdminsController::class, 'registerAdmin'])->name('admins.register');
+    Route::get('/admins/edit', [AdminsController::class, 'editAdmin'])->name('admins.edit');
+    Route::get('/admins/show', [AdminsController::class, 'showAdmins'])->name('admins.show');
+
+    //For Fees
     Route::get('/fees/show', [FeesController::class, 'showFees'])->name('fees.show');
     Route::get('/fees/edit',[FeesController::class,'updateRegisteredFees'])->name('fees.edit');
 
     //For Areas
     Route::get('/areas/show', [AreasController::class, 'showAreas'])->name('areas.show');
     Route::get('/areas/edit',[AreasController::class,'editRegisteredAreas'])->name('areas.edit');
-<<<<<<< HEAD
-    Route::get('/default/manegeAttributes', [AttributesController::class, 'default'])->name('default');
-=======
-    Route::get('/areas/register',[AreasController::class,'showRegisterArea'])->name('areas.register');
 
     //For Reservations
     Route::get('/reservations/show', [ReservationsController::class, 'showReservations'])->name('reservations.show');
->>>>>>> main
 });
-
-Route::get('/test/registration', [RegisterController::class, 'showRegistrationFormTest']);
