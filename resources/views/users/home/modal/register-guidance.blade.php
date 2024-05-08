fb067d3eaaf1239d51b75710101354600c7cb68c
YOSHIMI <lpit5vg4@gmail.com>
2024-05-08 22:34:19 +0900

    [backend/user/register/registerGuidance] - Added register route
    Added:
    register route in modal

--- resources/views/users/home/modal/register-guidance.blade.php
+++ resources/views/users/home/modal/register-guidance.blade.php
@@ -3,15 +3,15 @@
         <div class="modal-content">
             <div class="modal-header modal-head-color">
                 <h2 class="text-white mx-auto">New to this website?</h2>
-                <button type="button" class="btn-close btn-sm ms-auto" data-bs-dismiss="modal" style="filter: invert(1);"></button>
+                <button type="button" class="btn-close btn-sm ms-auto" data-bs-dismiss="modal"
+                    style="filter: invert(1);"></button>
             </div>
             <div class="modal-body mx-4 d-flex align-items-center" style="height: 200px">
                 <div>
                     <p>
                         If you want to make a reservation, please register as a member.
                     </p>
-                    {{-- Change to a href="#" to a href="{{ route('register') }}" later --}}
-                    <a href="#" class="btn btn-blue w-50 fw-bold">Register</a>
+                    <a href="{{ route('register') }}" class="btn btn-blue w-50 fw-bold">Register</a>
                 </div>
             </div>
         </div>
