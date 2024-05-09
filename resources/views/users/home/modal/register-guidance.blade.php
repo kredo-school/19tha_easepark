c026d6bc4b64616bc5c855c79754f92824e3fefd
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-05-09 21:23:42 +0900

    【backend/user/home/searchAvailableSpaces】Added Button Color for Login Updates: - style.css - register-guidance.blade.php

--- resources/views/users/home/modal/register-guidance.blade.php
+++ resources/views/users/home/modal/register-guidance.blade.php
@@ -2,18 +2,17 @@
     <div class="modal-dialog text-center modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header modal-head-color">
-                <h2 class="text-white mx-auto">New to this website?</h2>
+                <h2 class="text-white mx-auto">Welcome to our website!</h2>
                 <button type="button" class="btn-close btn-sm ms-auto" data-bs-dismiss="modal" style="filter: invert(1);"></button>
             </div>
             <div class="modal-body mx-4 d-flex align-items-center" style="height: 200px">
                 <div>
                     <p>
-                        If you want to make a reservation, please register as a member.
+                        To make a reservation, please register as a member or login if you already have an account.
                     </p>
-                    {{-- Change to a href="#" to a href="{{ route('register') }}" later --}}
                     <div class="d-flex justify-content-center">
                         <a href="{{route('register')}}" class="btn btn-blue fw-bold me-4">Register</a>
-                        <a href="{{route('login')}}" class="btn btn-blue fw-bold">Login</a>
+                        <a href="{{route('login')}}" class="btn btn-outline-blue fw-bold">Login</a>
                     </div>
                 </div>
             </div>
