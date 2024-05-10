33385990b745f30233ff3349742ec62eeaa4d3e1
Fuyuka Suzuki <nickinicki.m@gmail.com>
2024-05-10 21:12:26 +0900

    [backend/admin/manageAdmins/showRegisteredAdmins] - Added search function - Added pagination

--- app/Http/Controllers/admin/AdminsController.php
+++ app/Http/Controllers/admin/AdminsController.php
@@ -5,15 +5,32 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Admin;
+
 class AdminsController extends Controller
 {
+    private $admin;
+
+    public function __construct(Admin $admin)
+    {
+        $this->admin = $admin;
+    }
+
     public function registerAdmin() {
         return view('admin.admins.register');
     }
     public function editAdmin() {
         return view('admin.admins.edit');
     }
-    public function showAdmins() {
-        return view('admin.admins.show');
+    public function showAdmins(Request $request) {
+        if($request->search) {
+            $admins = $this->admin
+            ->where('name', 'like', '%' . $request->search . '%')
+            ->paginate(5);
+        } else {
+            $admins = $this->admin->paginate(5);
+        }
+
+        return view('admin.admins.show',['admins'=>$admins])->with('search', $request->search);
     }
 }
